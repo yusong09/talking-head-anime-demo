ce0cc17216d6b6408f8c43558735d2d898cf477a
Yusong Wang <yusong.wang@thoughtworks.com>
2021-06-14 15:40:02 +0800

    反复播放

--- app/manual_poser.py
+++ app/manual_poser.py
@@ -28,46 +28,53 @@ class ManualPoserApp:
         self.posed_image = None
         self.current_pose = None
         self.last_pose = None
-        self.needs_update = False
-        self.load_image()
-        # self.master.after(1000 // 30, self.update_image)
+        self.count = 0
+        self.TOTAL_IMAGE: int = 39
 
     def load_image(self):
         file_name = "data/illust/waifu_00_256.png"
         if len(file_name) > 0:
             image = PIL.Image.open(file_name)
-            # image = PhotoImage(file=file_name)
             if image.size[0] != self.poser.image_size() or image.size[1] != self.poser.image_size():
                 message = "The loaded image has size %dx%d, but we require %dx%d." \
                           % (image.size[0], image.size[1], self.poser.image_size(), self.poser.image_size())
                 print(message)
-
             self.source_image = extract_pytorch_image_from_filelike(file_name).to(self.torch_device).unsqueeze(dim=0)
-            self.needs_update = True
+
+    def main_loop(self):
+        self.load_image()
+        while self.count <= self.TOTAL_IMAGE:
             self.update_image()
+            self.count += 1
+        get_gif("result/result", self.TOTAL_IMAGE, 0.5)
 
     def update_pose(self):
-        self.current_pose = torch.zeros(self.pose_size, device=self.torch_device)
+        if self.current_pose is None:
+            self.current_pose = torch.zeros(self.pose_size, device=self.torch_device)
         for i in range(self.pose_size):
-            # self.current_pose[i] = self.param_sliders[i].get()
-            self.current_pose[i] = 1.0
-        self.current_pose = self.current_pose.unsqueeze(dim=0)
+            self.current_pose[i] += 0.02
+        return self.current_pose.unsqueeze(dim=0)
 
     def update_image(self):
         self.update_pose()
-        if (not self.needs_update) and self.last_pose is not None and (
-                (self.last_pose - self.current_pose).abs().sum().item() < 1e-5):
-            return
-        if self.source_image is None:
-            return
-        self.last_pose = self.current_pose
-
-        posed_image = self.poser.pose(self.source_image, self.current_pose).detach().cpu()
+        posed_image = self.poser.pose(self.source_image, self.update_pose()).detach().cpu()
         numpy_image = rgba_to_numpy_image(posed_image[0])
         pil_image = PIL.Image.fromarray(numpy.uint8(numpy.rint(numpy_image * 255.0)), mode='RGBA')
-        pil_image.save('result.png')
+        res_file = "result/result_%d.png" % self.count
+        pil_image.save(res_file)
 
-        self.needs_update = False
+
+def get_gif(pics_dir, n, t=0.1):
+    imgs = []
+    for i in range(n):
+        pic_name = '{}_{}.png'.format(pics_dir, i)
+        temp = PIL.Image.open(pic_name)
+        imgs.append(temp)
+    for img in reversed(imgs):
+        imgs.append(img)
+    save_name = '{}.gif'.format(pics_dir)
+    imgs[0].save(save_name, save_all=True, append_images=imgs, duration=t, loop=0)
+    return save_name
 
 
 if __name__ == "__main__":
@@ -80,6 +87,5 @@ if __name__ == "__main__":
         combine_module_spec=CombinerSpec(),
         combine_module_file_name="data/combiner.pt",
         device=cuda)
-    # root = Tk()
     app = ManualPoserApp(poser=poser, torch_device=cuda)
-    # root.mainloop()
+    app.main_loop()
